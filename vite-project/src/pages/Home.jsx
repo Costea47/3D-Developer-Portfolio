038f513976a5d5be9fc15bd19ee5cbff7d4e55d1
Constantin <horghidanconstantin91@yahoo.com>
2024-07-23 23:23:04 +0100

    used useFrame & useEffect to make the island move with mouse and keywords as well

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Suspense } from 'react'
 import { Canvas} from '@react-three/fiber'
 import Loader from '../components/Loader'
@@ -8,13 +8,14 @@ import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () =>
   {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0
+    let rotation = [0.1, 4.7, 0]
 
-    ]
     if(window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
       
@@ -25,12 +26,31 @@ const Home = () => {
     return [screenScale, screenPosition, rotation]
   }
 
+  const adjustPlaneForScreenSize = () =>
+    {
+      let screenScale, screenPosition;
+      
+  
+      if(window.innerWidth < 768) {
+        screenScale = [1.5, 1.5, 1.5];
+        screenPosition = [0, 1.5, 0]
+        
+      } else {
+        screenScale = [3, 3, 3];
+        screenPosition = [0, -4, -4]
+
+        
+      }
+      return [screenScale, screenPosition, ]
+    }
+
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h h-screen relative">
       {/* <div className=" absolute top-28 left-0 right-0 z-10 flex items-center justify-center">POPUP</div> */}
-      <Canvas className='w-full h-screen bg-transparent'
+      <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing': 'cursor-grab'}`}
       camera={{near: 0.1, far:1000}}>
         <Suspense fallback={<Loader />}>
         <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -42,8 +62,15 @@ const Home = () => {
         position = {islandPosition}
         scale = { islandScale}
         rotation = {islandRotation}
+        isRotating = {isRotating}
+        setIsRotating={setIsRotating}
         />
-        <Plane />
+        <Plane 
+        isRotating={isRotating}
+        planeScale={planeScale}
+        planePosition={planePosition}
+        rotation={[0, 20, 0]}
+        />
 
         </Suspense>
 
