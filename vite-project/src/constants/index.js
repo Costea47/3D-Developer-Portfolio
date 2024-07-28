fcb881d0ba81ec38b995f65f680afad929cdb595
Constantin <horghidanconstantin91@yahoo.com>
2024-07-28 11:36:58 +0100

    added description and skills with logos on aboutme page

---
+++ vite-project/src/constants/index.js
@@ -0,0 +1,104 @@
+import {
+  contact,
+  css,
+  git,
+  github,
+  html,
+  javascript,
+  python,
+  linkedin,
+  mongodb,
+  nextjs,
+  nodejs,
+  react,
+  tailwindcss,
+  typescript,
+  bootstrap,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: python,
+    name: "Python",
+    type: "Backendn",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: bootstrap,
+    name: "Bootstrap",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/Costea47",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/constantin-horghidan-7a0702150/",
+  },
+];
--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Constantin</span>
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+        a London-based junior software developer
+        with a passion for technology.My interests include exploring new tech trends, coding innovative solutions, and continuously expanding my skill set to stay ahead in the ever-evolving world of software development.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill)=>(
+            <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl'/>
+              <div className='btn-front rounded-xl flex justify-center intems-center'>
+                <img 
+                src={skill.imageUrl} 
+                alt={skill.name}
+                className='w-1/2 h-1/2 object-contain'
+                 />
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
